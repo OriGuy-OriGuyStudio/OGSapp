0ac26f6ef542e756756a502a5c1e11092ddc718e
Ori Guy Studio <origuy@origuystudio.com>
2025-02-26 18:19:53 +0200

    Refactor firebaseConfig.ts to remove unused code and simplify initialization

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app"
 import { initializeAuth, getReactNativePersistence } from "firebase/auth"
-import AsyncStorage from "@react-native-async-storage/async-storage"
 import { getFirestore } from "firebase/firestore"
 import { getStorage } from "firebase/storage"
 
@@ -16,9 +15,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig)
-export const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(AsyncStorage),
-})
+export const auth = initializeAuth(app)
 export const db = getFirestore(app)
 export const storage = getStorage(app)
 export default app
